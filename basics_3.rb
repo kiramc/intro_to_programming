c746d42bbfafaa0f16d76a7d7528b1a863670c5c
Kira McLean <kiraemclean@gmail.com>
2015-09-08 13:02:54 -0400

    do exercise basics 3

---
+++ basics_3.rb
@@ -0,0 +1,12 @@
+movies = {:pulp_fiction => 1994, 
+  :reservoir_dogs => 1992, 
+  :django_unchained => 2012, 
+  :inglorious_basterds => 2009, 
+  :the_hateful_eight => 2015, 
+  :jackie_brown =>1997}
+puts movies[:pulp_fiction]
+puts movies[:django_unchained]
+puts movies[:inglorious_basterds]
+puts movies[:the_hateful_eight]
+puts movies[:jackie_brown]
+
