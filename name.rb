fc2ae157562ce9f59fea03e4ec57c1c1aa6ab5e8
Kira McLean <kiraemclean@gmail.com>
2015-09-10 20:50:25 -0400

    finish variables

--- name.rb
+++ name.rb
@@ -1,3 +1,9 @@
 puts "What is your name?"
 name = gets.chomp
-puts name + " is super awesome!"
+puts "What about your last name?"
+last_name = gets.chomp
+puts name + " " + last_name
+
+10.times do
+  puts name
+end
